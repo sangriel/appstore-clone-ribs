255d7f90d40740f99431c93e288dc6cfef86b31f
sangriel <39114237+sangriel@users.noreply.github.com>
2024-11-28 20:14:42 +0900

    ModernRibs 프레임워크 적용 (#2)
    * moderRibs 프레임워크 적용
    
    * 쓸데 없는 코드 삭제

---
+++ AppStoreRibs/Presentation/App+SceneDelegate/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  AppStoreRibs
+//
+//  Created by sangmin han on 11/24/24.
+//
+
+import UIKit
+import ModernRIBs
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    private var launchRouter : LaunchRouting?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        let launchRouter = AppRootBuilder(dependency: AppComponent()).build()
+        self.launchRouter = launchRouter
+        
+        launchRouter.launch(from: window!)
+    }
+}
+
