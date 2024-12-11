acad80e255f7b4254a8a63bc6c43b9c7b23003db
sangriel <sangriel@naver.com>
2024-12-11 22:17:15 +0900

    save 함수 수정

--- AppStoreRibs/Data/Repositories/DefaultSearchPersistanceRepository.swift
+++ AppStoreRibs/Data/Repositories/DefaultSearchPersistanceRepository.swift
@@ -11,11 +11,7 @@ import Combine
 
 
 final class DefaultSearchPersistanceRepository : SearchPersistanceRepository {
-    
-    enum EntityName : String {
-        case RecentSearchWordEntity
-    }
-    
+   
     private let persistanceStorage : CoreDataStorage = CoreDataStorage.shared
     
     func getRecentSearchWord(term: String) -> AnyPublisher<[RecentSearchWord], any Error> {
@@ -23,13 +19,11 @@ final class DefaultSearchPersistanceRepository : SearchPersistanceRepository {
             .eraseToAnyPublisher()
     }
     
-    
     func saveRecentSearchWord(term: String) {
         guard term.isEmpty == false else  { return }
         persistanceStorage.performBackgroundTask { context in
-            let entity = NSEntityDescription.entity(forEntityName: EntityName.RecentSearchWordEntity.rawValue, in: context)!
-            let newData = NSManagedObject(entity: entity, insertInto: context)
-            newData.setValue(term, forKeyPath: "searchWord")
+            let entity = RecentSearchWordEntity(context: context)
+            entity.term = term
             do {
                 try context.save()
             } catch let error as NSError {
---
+++ RecentSearchWordEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  RecentSearchWordEntity+CoreDataClass.swift
+//  AppStoreRibs
+//
+//  Created by sangmin han on 12/11/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(RecentSearchWordEntity)
+public class RecentSearchWordEntity: NSManagedObject {
+
+}
